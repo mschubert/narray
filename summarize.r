6fe2bb5e9bb4aec33bbc17bf8325cbc9d495ba5d
mschubert <mschu.dev@gmail.com>
2015-08-12 13:10:40 +0100

    use biomart for lincs annotations

--- summarize.r
+++ summarize.r
@@ -2,7 +2,6 @@
 .s = import('./split')
 .m = import('./map')
 .bi = import('./bind')
-`%>%` = magrittr::`%>%`
 
 #' Summarize a matrix analogous to a grouped df in dplyr
 #'
@@ -10,10 +9,11 @@
 #' @param from   Names that match the dimension `along`
 #' @param to     Names that this dimension should be summarized to
 #' @param along  Along which axis to summarize
+#' @param ...    Parameters passed to `base/match`
 #' @param FUN    Which function to apply, default is throwing error on aggregation
 #' @return       A summarized matrix as defined by `from`, `to`
-summarize = function(x, along=1, to, from=dimnames(x)[[along]], FUN=aggr_error) {
-    lookup = .b$match(dimnames(x)[[along]], from=from, to=to, na_rm=TRUE)
+summarize = function(x, along=1, to, from=dimnames(x)[[along]], ...,  FUN=aggr_error) {
+    lookup = .b$match(dimnames(x)[[along]], from=from, to=to, ..., na_rm=TRUE)
     x = subset(x, index=names(lookup), along=along)
     
     # aggregate the rest using fun
