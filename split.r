9062f268def25857880fdba5c74a2326f02a8316
mschubert <mschu.dev@gmail.com>
2015-10-11 20:35:08 +0100

    use array module for splitting

--- split.r
+++ split.r
@@ -3,13 +3,14 @@
 #' Splits and array along a given axis, either totally or only subsets
 #'
 #' @param X        An array that should be split
-#' @param along    Along which axis to split
+#' @param along    Along which axis to split; use -1 for highest dimension
 #' @param subsets  Whether to split each element or keep some together
 #' @return         A list of arrays that combined make up the input array
 split = function(X, along, subsets=c(1:dim(X)[along]), drop=FALSE) {
     if (!is.array(X) && !is.vector(X) && !is.data.frame(X))
         stop("X needs to be either vector, array or data.frame")
-#    .check$all(X, along, subsets, x.to.array=FALSE) # breaks data.frame
+    if (along == -1)
+        along = length(dim(X))
 
     usubsets = unique(subsets)
     lus = length(usubsets)
