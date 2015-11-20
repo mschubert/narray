2ab36212046595a1268e2853d119b566136d90c0
mschubert <mschu.dev@gmail.com>
2015-11-20 16:07:58 +0000

    fix factor df subset + add test

--- subset.r
+++ subset.r
@@ -6,7 +6,10 @@
 #' @return       The subset of the array
 subset = function(X, index, along=NULL, drop=FALSE) {
     if (!is.list(index)) {
-        ldaX = length(dim(as.array(X)))
+        if (is.data.frame(X))
+            ldaX = length(dim(X))
+        else
+            ldaX = length(dim(as.array(X)))
         tmp = rep(list(TRUE), ldaX)
         if (is.null(along))
             along = ldaX
@@ -21,12 +24,16 @@ if (is.null(module_name())) {
     A = matrix(1:10, ncol=2)
     b = 1:5
     C = matrix(rnorm(20), dimnames=list(letters[1:5],LETTERS[1:4]), nrow=5, ncol=4)
+    DF = as.data.frame(lapply(as.data.frame(A), as.factor))
 
     testthat::expect_equal(subset(A, c(1,2), along=1), A[1:2,])
     testthat::expect_equal(subset(A, c(1,2), along=2), A)
 
     testthat::expect_equal(subset(b, 1:5 %in% c(2,4,5), along=1), subset(A[,1], c(2,4,5)))
     testthat::expect_error(subset(b, 1, along=2))
+
+    testthat::expect_equal(subset(DF, c(1,2), along=1), DF[1:2,])
+    testthat::expect_equal(subset(DF, c(1,2), along=2), DF)
 
     testthat::expect_equal(C[,2:3],
                            subset(C, 2:3, along=2, drop=TRUE),
