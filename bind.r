9199543d85c41dfa579ce189bc686d0b1cb249b9
mschubert <mschu.dev@gmail.com>
2016-01-16 13:15:46 +0000

    methods hack not needed for bind (#73)

--- bind.r
+++ bind.r
@@ -1,13 +1,9 @@
 #' Binds arrays together disregarding names
 #'
 #' @param arrayList  A list of n-dimensional arrays
-#' @param along      Along which axis to bind them together
+#' @param along      Along which axis to bind them together; default: new axis
 #' @return           A joined array
 bind = function(arrayList, along=length(dim(arrayList[[1]]))+1) {
-    # this import is required because package:abind references
-    # methods:Quote without importing it
-    library(methods)
-
     if (along == 1) # abind does not handle these cases well
         do.call(rbind, arrayList)
     else if (along == 2)
