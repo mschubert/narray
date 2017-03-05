896ab1e387fb32511e3a0c3fbd25483490c4fe87
mschubert <mschu.dev@gmail.com>
2017-03-05 21:06:35 +0000

    bind vectors as stack (#7)

--- R/bind.r
+++ R/bind.r
@@ -5,6 +5,24 @@
 #' @return           A joined array
 #' @export
 bind = function(arrayList, along=length(dim(arrayList[[1]]))+1) {
+    #TODO: this is borrowed from stack, refactor
+    # for vectors: if along=1 row vecs, along=2 col vecs, etc.
+    if (all(is.null(unlist(lapply(arrayList, base::dim))))) {
+        if (along == 1)
+            arrayList = lapply(seq_along(arrayList), function(i) {
+                re = t(as.matrix(arrayList[[i]]))
+                rownames(re) = names(arrayList)[i]
+                re
+            })
+        else if (along == 2)
+            arrayList = lapply(seq_along(arrayList), function(i) {
+                re = as.matrix(arrayList[[i]])
+                colnames(re) = names(arrayList)[i]
+                re
+            })
+    }
+    # end TODO:
+
     re = do.call(function(...) abind::abind(..., along=along), arrayList)
 
     # is.null(...) required because R is stupid
