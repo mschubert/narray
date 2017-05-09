7afc98d7afbbc464f49e3ccee7061e2e66deb5b7
mschubert <mschu.dev@gmail.com>
2017-05-09 18:33:25 +0100

    rudimentary support for along in mask (#8)

--- R/mask.r
+++ R/mask.r
@@ -1,12 +1,20 @@
 #' Converts a list of character vectors to a logical matrix
 #'
-#' @param x  A list of character vectors
-#' @return   A logical occurrence matrix
+#' @param x      A list of character vectors
+#' @param along  Which axis to spread mask on
+#' @return       A logical occurrence matrix
 #' @export
-mask = function(x) {
+mask = function(x, along=2) {
     if (is.factor(x))
         x = as.character(x)
 
-    vectorList = lapply(x, function(xi) stats::setNames(base::rep(TRUE, length(xi)), xi))
-    t(stack(vectorList, fill=FALSE))
+    list2logic = function(xi) stats::setNames(base::rep(TRUE, length(xi)), xi)
+
+    vectorList = lapply(x, list2logic)
+    stacked = stack(vectorList, fill=FALSE)
+
+    if (along == 1)
+        stacked
+    else
+        t(stacked)
 }
--- tests/testthat/test_mask.r
+++ tests/testthat/test_mask.r
@@ -11,5 +11,9 @@ test_that("mask", {
 
     Zref = structure(c(TRUE, TRUE, FALSE, TRUE, FALSE, TRUE), .Dim = c(3L,
     2L), .Dimnames = list(c("a", "b", "c"), c("e1", "e2")))
-    expect_equal(Z, Zref)
+
+    expect_equal(Z, Zref,
+                 mask(Z, along=2),
+                 t(mask(Z, along=1)),
+                 tolerance=1e-5, scale=1)
 })
