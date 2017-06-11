36e40333ffbaf8fb8ade799fd603d5feee1253b6
mschubert <mschu.dev@gmail.com>
2017-06-11 22:37:24 +0100

    split intersect in separate files

--- R/intersect.r
+++ R/intersect.r
@@ -48,24 +48,3 @@ intersect = function(..., along=1, envir=parent.frame(), drop=FALSE,
         for (name in names(dots))
             assign(name, dots[[name]], envir=envir)
 }
-
-#' Intersects a lits of arrays for common dimension names
-#'
-#' @param l.     List of arrays to perform operations on
-#' @param along  The axis along which to intersect
-#' @param drop   Drop unused dimensions on result
-#' @param fail_if_empty  Stop if intersection yields empty set
-#' @export
-intersect_list = function(l., along=1, drop=FALSE, fail_if_empty=TRUE) {
-    if (!is.list(l.))
-        stop("`intersect_list()` expects a list as first argument, found: ",
-             class(l.))
-
-    red_int = function(...) Reduce(base::intersect, list(...))
-
-    common = do.call(red_int, dimnames(l., along=along))
-    if (length(common) == 0 && fail_if_empty)
-        stop("Intersection is empty and fail_if_empty is TRUE")
-
-    lapply(l., function(e) subset(e, index=common, along=along, drop=drop))
-}
