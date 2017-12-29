dc0b798f868e9ddf422541047c57f76039f4bc7c
mschubert <mschu.dev@gmail.com>
2017-12-29 16:53:00 +0100

    rename summarize > translate

--- R/translate.r
+++ R/translate.r
@@ -1,17 +1,14 @@
-#' Summarize a matrix analogous to a grouped df in dplyr
+#' Translate an axis between two sets of identifiers
 #'
 #' @param x      A matrix
 #' @param from   Names that match the dimension `along`
 #' @param to     Names that this dimension should be summarized to
 #' @param along  Along which axis to summarize
-#' @param ...    Parameters passed to `base/match`
+#' @param ...    Parameters passed to `match`
 #' @param FUN    Which function to apply, default is throwing error on aggregation
 #' @return       A summarized matrix as defined by `from`, `to`
 #' @export
-summarize = function(x, along=1, to, from=dimnames(x)[[along]], ...,  FUN) {
-    warning("summarize() is deprecated and will be removed in 0.4.0. ",
-            "Use map() with subsets instead.")
-
+translate = function(x, along=1, to, from=dimnames(x)[[along]], ...,  FUN) {
     lookup = match(dimnames(x)[[along]], from=from, to=to, ..., na_rm=TRUE)
     x = subset(x, index=names(lookup), along=along)
     
--- tests/testthat/test-translate.r
+++ tests/testthat/test-translate.r
@@ -1,10 +1,10 @@
-context("summarize")
+context("translate")
 
 test_that("mean of same names", {
     G = matrix(c(1,2,0,3,4,5), nrow=3,
                dimnames=list(c('A','B','C'), c('D','E')))
 
-    expect_warning(W <- summarize(G, along=1, from=rownames(G), to=c('a','b','b'), FUN=mean))
+    W = translate(G, along=1, from=rownames(G), to=c('a','b','b'), FUN=mean)
     #   D   E
     # a 1 3.0
     # b 1 4.5
