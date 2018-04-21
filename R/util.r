247f3b8e3651dd44aef3e8f60f4c9394bec5702e
mschubert <mschu.dev@gmail.com>
2018-04-21 21:25:55 +0200

    let progress determine bar width

--- R/util.r
+++ R/util.r
@@ -2,8 +2,7 @@
 pb = function(ticks) progress::progress_bar$new(
     total = ticks,
     format = ":spin [:bar] :percent eta: :eta",
-    show_after = 5,
-    width = 20
+    show_after = 5
 )
 
 #' If no dimnames, return NULL and not list of NULLs
