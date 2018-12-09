74a2c3b2bb99c4552e2f888e33e88eb14860bc93
mschubert <mschu.dev@gmail.com>
2018-12-09 17:59:31 +0100

    save index mapping for all arrays
    mem size is much smaller than result anyway

--- src/stack.cpp
+++ src/stack.cpp
@@ -9,6 +9,7 @@ SEXP cpp_stack(SEXP arlist) {
     auto ar = as<List>(arlist);
     auto dimnames = vector<vector<string> >();
     auto axmap = vector<unordered_map<string, int> >();
+    auto a2r = vector<vector<vector<int> > >(ar.size());
 
     // create lookup tables for all present dimension names
     for (int a=0; a<Rf_xlength(ar); a++) { // array in arlist
@@ -16,6 +17,7 @@ SEXP cpp_stack(SEXP arlist) {
         auto dn = as<List>(va.attr("dimnames"));
         auto da = as<vector<int> >(va.attr("dim"));
 
+        a2r[a] = vector<vector<int> >(da.size());
         if (dimnames.size() < da.size()) {
             dimnames.resize(da.size());
             axmap.resize(da.size());
@@ -30,6 +32,7 @@ SEXP cpp_stack(SEXP arlist) {
                     axmap[d].emplace(dni[e], axmap[d].size());
                     dimnames[d].push_back(dni[e]);
                 }
+                a2r[a][d].push_back(axmap[d][dni[e]]);
             }
         }
     }
@@ -48,26 +51,13 @@ SEXP cpp_stack(SEXP arlist) {
     // fill the result array
     for (int a=0; a<Rf_xlength(ar); a++) { // array in arlist
         auto va = as<NumericVector>(ar[a]);
-        auto dn = as<List>(va.attr("dimnames"));
-        auto da = as<vector<int> >(va.attr("dim"));
-
-        // numeric axes index mapping from current to result array
-        auto a2r = vector<vector<int> >(rdim.size());
-        for (int d=0; d<da.size(); d++) { // dimension in array
-            auto dni = as<vector<string> >(dn[d]); // -> maybe keep SEXP later if indexable
-
-            for (int e=0; e<da[d]; e++) { // element in dimension
-                auto v = dni[e];
-                a2r[d].push_back(axmap[d][v]);
-            }
-        }
-
-        auto aidx = vector<int>(da.size()); // axes in orig array
-        auto ridx = vector<int>(da.size()); // axes in result array
+        auto aidx = vector<int>(a2r[a].size()); // axes in orig array
+        auto ridx = vector<int>(a2r[a].size()); // axes in result array
         for (int d=1; d<aidx.size(); d++)
-            ridx[d] = a2r[d][0];
+            ridx[d] = a2r[a][d][0];
+
         for (int e=0; e<va.size(); e++) {
-            ridx[0] = a2r[0][aidx[0]];
+            ridx[0] = a2r[a][0][aidx[0]];
             int ridx_flat = inner_product(rdim.begin(), rdim.end()-1,
                     ridx.begin()+1, 0) + ridx[0];
             result[ridx_flat] = va[e];
@@ -80,10 +70,10 @@ SEXP cpp_stack(SEXP arlist) {
 
             aidx[0]++;
             for (int d=0; d<aidx.size()-1; d++) {
-                if (aidx[d] == da[d]) {
+                if (aidx[d] == a2r[a][d].size()) {
                     aidx[d] = 0;
-                    ridx[d] = a2r[d][0];
-                    ridx[d+1] = a2r[d+1][++aidx[d+1]];
+                    ridx[d] = a2r[a][d][0];
+                    ridx[d+1] = a2r[a][d+1][++aidx[d+1]];
                 } else
                     break;
             }
