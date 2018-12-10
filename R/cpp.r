e46277a8908a511a47734d92eec66291efe1da4e
mschubert <mschu.dev@gmail.com>
2018-12-10 20:49:19 +0100

    add Rcpp specifics to new pkg doc file

--- R/cpp.r
+++ R/cpp.r
@@ -1,7 +1,6 @@
 #' test
 #'
 #' @export
-#' @useDynLib narray
 cpp = function() {
     A = matrix(1:4, nrow=2, ncol=2, dimnames=list(c('a','b'),c('x','y')))
     B = matrix(5:6, nrow=2, ncol=1, dimnames=list(c('b','a'),'z'))
---
+++ R/narray-package.r
@@ -0,0 +1,11 @@
+#' R package for subset- and name-aware array utility functions
+#'
+#' Stacking arrays according to dimension names, subset-aware splitting and
+#' mapping of functions, intersecting along arbitrary dimensions, converting to
+#' and from data.frames, and many other helper functions.
+#'
+#' @name narray
+#' @docType package
+#' @useDynLib narray
+#' @import Rcpp
+NULL
